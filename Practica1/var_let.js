a9a8e39f04673a6626674d911b6b50a1eaaf565b
GonzaaaXD <gonzalouribe49@gmail.com>
2025-05-16 20:14:32 -0600

    saludoPersonalizado

--- Practica1/var_let.js
+++ Practica1/var_let.js
@@ -1,5 +1,5 @@
 {
-   let nombre = "Aramando", edad=25; 
+   let nombre = "Armando", edad=25; 
    nombre = "Ana Maria";
    const saludo = "Hola, " + nombre + ". Tienes: " + edad + " años";
    console.log(saludo); 
@@ -7,4 +7,13 @@
 {
     const cuadrado = (numero) => numero * numero;
     console.log(cuadrado(8));
+}
+{
+    const saludoPersonalizado = () =>{
+        const nombre = "Gonzalo", edad=22;
+        const saludo = "Hola, " + nombre + ". Tienes: " + edad + " años";
+
+        console.log(saludo);
+    };
+    saludoPersonalizado();
 }
