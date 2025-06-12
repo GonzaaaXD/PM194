60940ece57b2228b6b40b4c1041dc9f1260d1b43
GonzaaaXD <gonzalouribe49@gmail.com>
2025-06-11 19:47:24 -0600

    fin_Practica4

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
 
-const Texto = (props) => {
-  const {contenido} = props
-  return(<Text>{contenido}</Text>)
-
+const Texto = () => {
+  const [contenido, setContenido] = useState('Hola mundo React Native');
+  const actualizaTexto = () => {
+    setContenido('Estado actualizado del texto');
+  };
+  return(<Text onPress={actualizaTexto}>{contenido}</Text>)
 };
+
+const Boton = () => {
+  const [titulo, setTitulo] = useState('Presionar');
+  const actualizarTitulo = () => {
+    setTitulo('Esto es ReactNative');
+  };
+  return(
+    <Button title={titulo} onPress={actualizarTitulo}></Button>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto contenido="hola"> </Texto>
-      <Texto contenido="mundo"> </Texto>
-      <Texto contenido="react native"> </Texto>
-      <Button title='Presionar'></Button>
+      <Texto></Texto>
+      <Texto></Texto>
+      <Texto></Texto>
+      <Boton></Boton>
       <StatusBar style="auto" />
     </View>
   );
