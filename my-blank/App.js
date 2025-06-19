1ad260f6150e2ff6e51863a033df343071cfc2e4
GonzaaaXD <gonzalouribe49@gmail.com>
2025-06-18 19:49:34 -0600

    boton

--- my-blank/App.js
+++ my-blank/App.js
@@ -2,34 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react';
 
+
 const Texto = ({style}) => {
   const [contenido, setContenido] = useState('Hola mundo React Native');
   const actualizaTexto = () => {
-    setContenido('Estado actualizado del texto');
+    setContenido('Hola mundo cómo estás?');
   };
-  return(<Text style={[styles.text, style]} onPress={actualizaTexto}>{contenido}</Text>)
+   return (
+    <View Style={{margin:10}}>
+    <Text Style={[styles.text. style]}>{contenido}</Text>
+    <Button title='actualizaTexto'onPress={actualizaTexto}color="purple"/>
+    </View>
+    );
 };
 
-/*const Boton = () => {
-  const [titulo, setTitulo] = useState('Presionar');
-  const actualizarTitulo = () => {
-    setTitulo('Esto es ReactNative');
-  };
-  return(
-    <Button title={titulo} onPress={actualizarTitulo}></Button>
-  )
-}*/
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Texto style={styles.red}></Texto>
+    
       <Texto style={styles.blue}></Texto>
       <Texto style={styles.green}></Texto>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -46,4 +45,4 @@ const styles = StyleSheet.create({
   red:{backgroundColor:'red'},
   blue:{backgroundColor:'blue'},
   green:{backgroundColor:'green'},
-});
+})
